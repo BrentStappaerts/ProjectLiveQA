1e0b5bf09196f8fa6672acaea28de346e18cf2b9
IGenerate <brent.stappaerts@gmail.com>
2015-05-06 15:08:04 +0200

    Route signup + signup page +css

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('test');
+});
+
+router.get('/signup', function(req, res, next) {
+   res.render('signup', { title: 'Express' });
 });
 
 module.exports = router;
